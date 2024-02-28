11b5c8ffba7bc2eaac14341f335f2ed0752f8904
SABHYA18 <sbhmhjn@gmail.com>
2024-02-29 01:12:03 +0530

    updated the overview.html and added the static middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,7 +5,7 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is : ${val}`);
+  // console.log(`Tour id is : ${val}`);
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -54,7 +54,7 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
