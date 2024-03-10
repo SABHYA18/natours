3819a8e4813eeeccf49620ee55c446bc0bfed41b
SABHYA18 <sbhmhjn@gmail.com>
2024-03-11 04:32:52 +0530

    sent the data to the database on the atlas.

--- server.js
+++ server.js
@@ -1,11 +1,52 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(app.get('env'));
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successfull !'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error💥💥: ', err);
+  });
 
 const port = process.env.PORT || 3000;
 // const port = 3000;
