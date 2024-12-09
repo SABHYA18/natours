c248722985ec134134f9416cc71578b8af4a7995
Sabhya <sabhya.mahajan@cashfree.com>
2024-12-09 23:29:07 +0530

    Security of the app is finished

--- app.js
+++ app.js
@@ -1,19 +1,66 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss= require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+// const deepSanitize = require('./utils/deepSanitize');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1)Global MIDDLEWARES
+// set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-app.use(express.json());
+// Limit requests from the same api
+const limiter = rateLimit({
+  max: 100, // set wrt to the requests recieved. can be 10000000
+  windowMs: 60 * 60 * 1000, 
+  message: "Too many requests from this IP. Please try again in an hour"
+})
+app.use('/api', limiter);
+
+// Body parser, reading data from the body into req.body
+app.use(express.json({
+  limit: '10kb'
+}));
+
+// data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against XSS (cross site scripting attacks)
+app.use(xss());
+
+/*
+app.use((req, res, next)=>{
+  req.body = deepSanitize(req.body);
+  next();
+});
+*/  //--> can provide extra layer of protection but the xss package can be used
+
+
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist: [
+    'duration','ratingsQuantity', 'ratingsAverage', 'maxGroupSize','difficulty',
+    'price'
+  ]
+}));
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(x); --> if there is any error in the express middleware, it goes to the main error handling middleware to rectify it.
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,16 @@ const signToken = function (id) {
 
 const createSendToken = (user, statusCode, res)=>{
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000),
+    httpOnly: true
+  };
+  if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  
+  // The password should not show up in the response while creating the user
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: "success",
     token, 
